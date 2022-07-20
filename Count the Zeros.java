631d110fe587d02076b6618025262592d8f059d0
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-07-20 19:39:47 +0530

    Create Count the Zeros.java

---
+++ Count the Zeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    int countZeroes(int[] arr, int n) {
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                count++;
+            }
+            
+            
+        }
+        return count;
+        
+        
+    }
+
+}
