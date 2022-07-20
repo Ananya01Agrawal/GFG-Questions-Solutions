9eece4ee0fd73e2fa2baa022ae5326eb253a426f
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-07-20 19:42:48 +0530

    Create Number of occurrence.java

---
+++ Number of occurrence.java
@@ -0,0 +1,10 @@
+class Solution {
+    int count(int[] arr, int n, int x) {
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x)
+                count++;
+        }
+        return count;
+    }
+}
