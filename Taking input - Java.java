b492f23198a63b1853ddc8643840d7aaf737f2ff
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-07-25 21:18:49 +0530

    Create Taking input - Java.java

---
+++ Taking input - Java.java
@@ -0,0 +1,25 @@
+class Geeks{
+    
+    // Function to take input using Scanner class
+    static void IOFunction(){
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int a=sc.nextInt();
+		    float b=sc.nextFloat();
+		    long c=sc.nextLong();
+		    byte d=sc.nextByte();
+		    sc.nextLine();
+		    String s=sc.nextLine();
+		    
+		
+		while(t-- > 0){
+		    System.out.println(a);
+		    System.out.println(b);
+		    System.out.println(c);
+		    System.out.println(d);
+		    System.out.println(s);
+		    }
+		
+    }
+    
+}
