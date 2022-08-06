f457873dd80fb0c30e316516e7bdb9aa3c6be91f
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-08-06 20:17:02 +0530

    Create Palindrome String.java

---
+++ Palindrome String.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    int isPalindrome(String S) {
+        int i=0;
+        int j=S.length()-1;
+        while(i<j)
+        {
+            if(S.charAt(i)!=S.charAt(j)){
+                return 0;
+            }
+            i++;
+            j--;
+        }
+        return 1;
+       
+    }
+};
