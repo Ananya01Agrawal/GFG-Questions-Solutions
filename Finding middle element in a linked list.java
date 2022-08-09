e4585a53c6ac79f9de29dfb7ac6ec5077658bc5f
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-08-09 13:39:08 +0530

    Create Finding middle element in a linked list.java

---
+++ Finding middle element in a linked list.java
@@ -0,0 +1,18 @@
+
+class Solution
+{
+    int getMiddle(Node head)
+    {
+        Node slow=head;
+        Node fast=head;
+        
+        while(fast!=null &&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+          
+         
+        }
+        return slow.data;
+         // Your code here.
+    }
+}
