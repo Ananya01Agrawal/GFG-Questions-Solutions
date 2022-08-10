58465c831d4d2acd0488cde5b60b7e88bf1f0ba2
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-08-10 15:45:08 +0530

    Create Consonants and Vowels check - Java.java

---
+++ Consonants and Vowels check - Java.java
@@ -0,0 +1,32 @@
+
+
+class Geeks{
+    
+    static void checkString(String s)
+    {
+        int v=0;
+       int c=0;
+       char x;
+       //Your code here
+       for(int i = 0; i < s.length(); i++){
+           x = s.charAt(i);
+           if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
+               v++;
+           }
+           else if(x==' '){
+               continue;
+           }
+           else{
+               c++;
+           }
+       }
+       if(v>c)
+       System.out.print("Yes");
+       else if(c>v)
+       System.out.print("No");
+       else
+      System.out.print("Same");
+       
+       System.out.println();
+   }
+}
