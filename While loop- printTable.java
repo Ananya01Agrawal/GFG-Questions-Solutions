1c103ee0daf8322adfa88cb501cd2b9f9c6282a6
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-08-29 11:48:48 +0530

    Create While loop- printTable.java

---
+++ While loop- printTable.java
@@ -0,0 +1,15 @@
+class Geeks {
+	 static void printTable (int n)
+	 {
+           int multiplier=10;
+           while(multiplier>1)
+           {
+              
+              System.out.print(multiplier*n + " ");
+                   multiplier--;
+           }
+      System.out.println(multiplier*n);
+ }
+
+}
+	
