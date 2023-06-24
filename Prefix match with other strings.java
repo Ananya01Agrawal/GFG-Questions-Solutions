3c03d255de25eae6509785dbbfd6e61b0309f03d
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-06-24 22:31:43 +0530

    Create Prefix match with other strings.java

---
+++ Prefix match with other strings.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int klengthpref(String[] arr, int n, int k, String str)
+    {
+        // code here
+        int cnt=0;
+        if(k>str.length())
+        return 0;
+    for(int i=0;i<n;i++)
+    {
+      if(arr[i].length()>=k&&arr[i].substring(0,k).equals(str.substring(0,k)))
+      cnt++;
+    }
+    return cnt;
+    }
+}
